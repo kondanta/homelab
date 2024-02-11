946024f3a8e8fb994c58f15d6902df291914f067
Taylan Dogan <git@taylandogan.info>
2024-02-11 02:17:17 +0100

    feat(killswitch): add shutdown command for windows
    Signed-off-by: Taylan Dogan <git@taylandogan.info>

--- services/killswitch/src/main.rs
+++ services/killswitch/src/main.rs
@@ -1,3 +1,16 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+
+    // Create a command to execute shutdown command
+    let command = Command::new("shutdown")
+        .arg("/s")
+        .arg("/t")
+        .arg("0")
+        .output();
+
+    match command {
+        Ok(_) => println!("Shutdown initiated"),
+        Err(e) => println!("Error initiating shutdown: {:?}", e),
+    }
 }
