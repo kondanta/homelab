3e86dbeb06bdaf73e700e9d858be587cd664cd8a
Taylan Dogan <git@taylandogan.info>
2024-02-11 03:53:26 +0100

    feat(killswitch): listen queue to catch shutdown
    Signed-off-by: Taylan Dogan <git@taylandogan.info>

--- services/killswitch/src/main.rs
+++ services/killswitch/src/main.rs
@@ -1,7 +1,23 @@
 use std::process::Command;
+use lib::{bus, dto::QueueType};
 
 fn main() {
+    let bus = bus::Bus::new();
+    let queue = QueueType::Killswitch;
+    bus.listen(
+        queue.to_string(),
+        Some(queue.channel_id()),
+        killer
+    ).unwrap();
+}
 
+fn killer(body: std::borrow::Cow<str>) {
+    if body.contains("shutdown") {
+        shutdown();
+    }
+}
+
+fn shutdown() {
     // Create a command to execute shutdown command
     let command = Command::new("shutdown")
         .arg("/s")
