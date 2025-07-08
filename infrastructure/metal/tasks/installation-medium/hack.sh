c72eda1010e3b25c449a75864e5439f3225b4455
Taylan Dogan <git@taylandogan.info>
2025-07-09 00:52:53 +0200

    chore: update installation medium
    Signed-off-by: Taylan Dogan <git@taylandogan.info>

--- infrastructure/metal/tasks/installation-medium/hack.sh
+++ infrastructure/metal/tasks/installation-medium/hack.sh
@@ -2,8 +2,14 @@
 
 # My current network is not smart enough for me to setup PXE booting. So I'm going to use a USB stick to boot the Talos installer.
 
-# Fetch the iso
-curl -L https://factory.talos.dev/image/97d3538093bb06a33cc2647552a0f88475a592e44bfd8326cd44b656b13f8bbc/v1.7.6/metal-amd64.iso -o talos.iso
+# Check if the ISO file already exists
+if [ -f "talos.iso" ]; then
+    echo "ISO file already exists. Skipping download."
+else
+    echo "ISO file not found. Downloading..."
+    curl -L https://factory.talos.dev/image/636aa4f93757babc9f57e2611552df92506ad9701629d1fee5b2c52de791abd2/v1.10.2/metal-amd64.iso -o talos.iso
+
+fi
 
 cat <<EOF > ./data/usb-stick-boot.cusomisation
 customization:
@@ -16,19 +22,21 @@ customization:
         - talos.shutdown=poweroff
     systemExtensions:
         officialExtensions:
-            - siderolabs/i915-ucode
+            - siderolabs/btrfs
+            - siderolabs/i915
             - siderolabs/intel-ice-firmware
             - siderolabs/intel-ucode
+            - siderolabs/mei
+            - siderolabs/nut-client
             - siderolabs/tailscale
             - siderolabs/util-linux-tools
-            - siderolabs/zfs
 EOF
 
-echo -n "SCHEMATIC_ID=\"97d3538093bb06a33cc2647552a0f88475a592e44bfd8326cd44b656b13f8bbc\"" > ./data/usb-stick-boot.env
+echo -n "SCHEMATIC_ID=\"636aa4f93757babc9f57e2611552df92506ad9701629d1fee5b2c52de791abd2\"" > ./data/usb-stick-boot.env
 
 # Check if the USB stick is mounted
-if [[ -L /dev/disk/by-id/usb-Sandisk_USB_Ultra_VC0494150622135591000513-0\:0 ]]; then
-    sudo dd bs=4M if=talos176.iso of=/dev/disk/by-id/usb-Sandisk_USB_Ultra_VC0494150622135591000513-0\:0 conv=fsync oflag=direct status=progress
+if [[ -L /dev/disk/by-id/usb-Innostor_Innostor_000000000000001222-0\:0 ]]; then
+    sudo dd bs=4M if=talos.iso of=/dev/disk/by-id/usb-Innostor_Innostor_000000000000001222-0\:0 conv=fsync oflag=direct status=progress
 else
     echo "USB stick not found"
 fi
