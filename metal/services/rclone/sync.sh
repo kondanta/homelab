53a77ba2a22ee7494cdd53da62ccc00b5fff347d
Taylan Dogan <git@taylandogan.info>
2026-05-28 23:12:06 +0200

    feat(metal): add rclone
    Signed-off-by: Taylan Dogan <git@taylandogan.info>

---
+++ metal/services/rclone/sync.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+echo "[$(date)] Starting sync"
+
+for PATH_SPEC in $SYNC_BUCKETS; do
+  echo "[$(date)] Syncing: $PATH_SPEC"
+  rclone sync "garage:$PATH_SPEC" "proton:homelab/$PATH_SPEC" -v
+  echo "[$(date)] Done: $PATH_SPEC"
+done
+
+echo "[$(date)] Sync complete"
