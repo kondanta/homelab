aaeb862b85f716794fb19458ad326dae7157625d
Taylan Dogan <git@taylandogan.info>
2026-06-08 18:08:22 +0200

    chore(rclone): exclude log files from sync
    Signed-off-by: Taylan Dogan <git@taylandogan.info>

--- metal/services/rclone/sync.sh
+++ metal/services/rclone/sync.sh
@@ -5,7 +5,7 @@ echo "[$(date)] Starting sync"
 
 for PATH_SPEC in $SYNC_BUCKETS; do
   echo "[$(date)] Syncing: $PATH_SPEC"
-  rclone sync "garage:$PATH_SPEC" "proton:homelab/$PATH_SPEC" -v
+  rclone sync "garage:$PATH_SPEC" "proton:homelab/$PATH_SPEC" -v --exclude '_log_*'
   echo "[$(date)] Done: $PATH_SPEC"
 done
 
