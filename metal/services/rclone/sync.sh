54e9d60b2babf2e9da1d2b9160d6932a51f2a8eb
Taylan Dogan <git@taylan.dev>
2026-08-03 16:39:03 +0200

    fix: change nodeexporter job name for metal machines
    Signed-off-by: Taylan Dogan <git@taylan.dev>

--- metal/services/rclone/sync.sh
+++ metal/services/rclone/sync.sh
@@ -5,7 +5,7 @@ echo "[$(date)] Starting sync"
 
 for PATH_SPEC in $SYNC_BUCKETS; do
   echo "[$(date)] Syncing: $PATH_SPEC"
-  rclone sync "garage:$PATH_SPEC" "proton:homelab/$PATH_SPEC" -v --exclude '_log_*'
+  rclone sync "garage:$PATH_SPEC" "proton:homelab/$PATH_SPEC" -v --exclude '_log_*' --delete-before
   echo "[$(date)] Done: $PATH_SPEC"
 done
 
